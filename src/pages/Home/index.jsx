02e8d7977dd7ff67bb5db6660fe6d972304a56b7
Luiz <gustavoooliveira111@gmail.com>
2022-04-08 19:19:39 -0300

    styling css page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,10 +2,10 @@ import './style.css'
 
 export function Home() {
   return (
-    <>
+    <div className='container'>
       <h1>Lista de presença</h1>
       <input type="text" placeholder="adicione um texto"/>
-      <button type="button">adiconar</button>
-    </>
+      <button type="button">Adicionar</button>
+    </div>
   )
 }
