f5a23e6524d69ee37135b0d019ca12feeeb3f1d2
Luiz <gustavoooliveira111@gmail.com>
2022-04-08 15:55:37 -0300

    organize files and import css

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export function Home() {
+  return (
+    <>
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="adicione um texto"/>
+      <button type="button">adiconar</button>
+    </>
+  )
+}
