78573afdaa9a3e1d298c9266fa36c57fa450f324
Luiz <gustavoooliveira111@gmail.com>
2022-04-09 13:02:11 -0300

    add a component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,21 @@
 import './style.css'
 
+import { Card } from '../../components/Card'
+
 export function Home() {
   return (
     <div className='container'>
+      <Card name="Marcos" time="21:33:54"/>
+      
       <h1>Lista de presença</h1>
       <input type="text" placeholder="adicione um texto"/>
       <button type="button">Adicionar</button>
+
+      <Card name="Luiz" time="20:13:34"/>
+      <Card name="Marcos" time="21:33:54"/>
+      
+      
+
     </div>
   )
 }
