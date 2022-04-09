e5e74d992509475ebc7e23ec37f8c660241e8d7e
Luiz <gustavoooliveira111@gmail.com>
2022-04-09 18:27:08 -0300

    adding dynamic components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,54 @@
+import React, { useState } from 'react';
+
 import './style.css'
 
 import { Card } from '../../components/Card'
 
 export function Home() {
+  //Estado que atualiza o nome dos alunos
+  const [studentName, setStudentName] = useState("Aluno")
+  //Estado que cria Cards para cada aluno
+  const [students, setStudents] = useState([])
+
+  //Função que cria um novo objeto com nome e hora atualizadas em um Card
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+
+    //Mantendo o conteúdo anterior usando o spread operator, e criando um novo vetor que contém o prevState e o objeto newStudent
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
   return (
     <div className='container'>
-      <Card name="Marcos" time="21:33:54"/>
+      <header>
+        <h1>Lista de presença</h1>
+        
+        <div>
+          <strong>Luiz Gustavo</strong>
+          <img src="https://github.com/LuizSig.png" alt="Foto de perfil" />
+        </div>
       
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="adicione um texto"/>
-      <button type="button">Adicionar</button>
+      </header>
+      
+      <input 
+      type="text" 
+      placeholder="adicione um texto" 
+      onChange={(e) => setStudentName(e.target.value)}/>
 
-      <Card name="Luiz" time="20:13:34"/>
-      <Card name="Marcos" time="21:33:54"/>
-      
-      
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+        </button>
 
+      {
+        students.map(student => <Card key={student.time} name={student.name} time={student.time}/>)
+      }
     </div>
   )
 }
